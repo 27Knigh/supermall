1330b7f8f389579b95112c0b95e595d2b019d388
27Knigh <1987691449@qq.com>
2021-01-20 16:17:40 +0800

    最后一次提交

---
+++ src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="features">
+        <a href="http://act.mogujie.com/zzlx67">
+            <img src="~assets/img/home/recommend_bg.jpg" alt="">
+        </a>
+    </div>
+</template>
+<script>
+export default {
+   name: "FeatureView",
+
+}
+</script>
+<style scoped>
+    .features img {
+        width: 100%;
+    }
+</style>>
+
